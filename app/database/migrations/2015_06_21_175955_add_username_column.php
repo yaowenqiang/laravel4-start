d9b2ec5aecedf053f9a505f23ac802b82bdee400
jack yao <yaowenqiang111@163.com>
2015-06-22 02:07:09 +0800

    add the basic migration files

---
+++ app/database/migrations/2015_06_21_175955_add_username_column.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUsernameColumn extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::table('users',function($table){
+            $table->string('username');
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::table('users',function($table){
+            $table->dropColumn('username');
+        });
+	}
+
+}
